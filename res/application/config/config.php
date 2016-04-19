9cc8fc4577a570ace9a7b9c60f97c79205fed61b
jhordy abonia <jhordyabonia@bitbucket.org>
2016-04-19 03:02:41 -0500

    ***********

--- res/application/config/config.php
+++ res/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://proveedorlinux0.cloudapp.net/';
+$config['base_url']	= 'http://proveedorlinux0.cloudapp.net/index.php';
 
 /*
 |--------------------------------------------------------------------------
