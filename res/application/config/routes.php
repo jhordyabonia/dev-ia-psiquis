198e8af3675c45441841cac765e932a4506196bd
jhordy abonia <jhordyabonia@bitbucket.org>
2016-04-19 22:35:28 -0500

    ***********

--- res/application/config/routes.php
+++ res/application/config/routes.php
@@ -42,8 +42,8 @@ $route['default_controller'] = "index";
 $route['404_override'] = '';
 $route['productos/pagina'] = 'productos';//cuando sea la primera página
 $route['productos/pagina/(:num)'] = 'productos';//cuando no sea la primera página
-$route['psiquis/(:any)'] = "index.php/psiquis/$1";
-$route['psiquis'] = "index.php/psiquis";
+$route['(:any)'] = "/psiquis/$1";
+#$route['index.php/psiquis/(:any)'] = "/psiquis/$1";
 
 
 /* End of file routes.php */
