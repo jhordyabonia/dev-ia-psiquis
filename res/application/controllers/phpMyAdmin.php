a65c40ad85d3757cde4ff4600d92c0a10ce9a9fe
jhordy abonia <jhordyabonia@bitbucket.org>
2016-04-22 01:30:19 -0500

    parche para trabajar phpMyAdmin

---
+++ res/application/controllers/phpMyAdmin.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('BASEPATH')) {
+	exit('No direct script access allowed');
+}
+
+class phpMyAdmin extends CI_Controller {
+    
+    function __construct() 
+    {
+		parent::__construct();
+    }
+    function index()
+    {
+        redirect(base_url().'uploads/phpMyAdmin');
+    }
+}
