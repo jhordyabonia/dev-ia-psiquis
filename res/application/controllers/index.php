b3c8e852d4b4d21df8f2f098f2f86f2baec38c35
jhordy abonia <jhordyabonia@bitbucket.org>
2016-04-23 19:01:22 -0500

    ******

--- res/application/controllers/index.php
+++ res/application/controllers/index.php
@@ -13,6 +13,6 @@ class Index extends CI_Controller
     {parent::__construct();}
     public function index()
     {
-        redirect('http://www.proveedor.com.co');
+        ##redirect('http://www.proveedor.com.co');
     }
 }
